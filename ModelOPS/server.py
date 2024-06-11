3e522e1f2bfeef56acff142c6aa29cc24b1cf897
Mihnea Lopataru <mihnealopataru@gmail.com>
2024-06-12 00:18:06 +0300

    Small improvements

--- ModelOPS/server.py
+++ ModelOPS/server.py
@@ -20,10 +20,12 @@ async def download_files(request: FileDownloadRequest):
     temporary_directory = tempfile.mkdtemp()
 
     try:
+        output_path = os.path.join(temporary_directory, 'data')
+
         await asyncio.get_event_loop().run_in_executor(
             None,
             minio_client.get_files_by_user_label,
-            request.bucket_name, request.user_id, request.label, temporary_directory
+            request.bucket_name, request.user_id, request.label, output_path
         )
 
         return {"message": "Files retrieved successfully.", "file_path": temporary_directory}
@@ -37,9 +39,10 @@ async def clean_files(request: FileCleaningRequest):
         if not os.path.exists(request.input_path):
             raise HTTPException(status_code=404, detail="Input path not found")
 
-        output_path = tempfile.mkdtemp()
+        input_path = os.path.join(request.input_path, 'data')
+        output_path = os.path.join(request.input_path, 'cleaned')
 
-        preprocess_files(request.input_path,
+        preprocess_files(input_path,
                          output_path,
                          request.patient_identifier,
                          request.encoding_method,
@@ -57,14 +60,29 @@ async def train_model(request: TrainModelRequest):
     temporary_directory = tempfile.mkdtemp()
 
     try:
-        random_forest.run(request.input_path,
+        if request.excluded_columns is None:
+            request.excluded_columns = []
+
+        if request.patient_identifier not in request.excluded_columns:
+            request.excluded_columns.append(request.patient_identifier)
+
+        default_directory = os.path.join(request.input_path, 'data')
+        cleaned_directory = os.path.join(request.input_path, 'cleaned')
+
+        if os.path.exists(cleaned_directory):
+            input_path = cleaned_directory
+        else:
+            input_path = default_directory
+
+        random_forest.run(input_path,
                           temporary_directory,
                           request.target_column,
                           request.max_depth,
                           request.random_state,
                           request.chunk_size,
                           request.excluded_columns)
-        statistical_analysis.analyze(request.input_path,
+
+        statistical_analysis.analyze(default_directory,
                                      temporary_directory,
                                      request.target_column,
                                      request.patient_identifier)
@@ -72,7 +90,6 @@ async def train_model(request: TrainModelRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     finally:
-        shutil.rmtree(temporary_directory)
         shutil.rmtree(request.input_path)
 
 if __name__ == "__main__":
